510eb3a605508d51dcaa5ee948250a4ad5175c0a
phi <philipwaritschlager@web.de>
2016-11-30 02:33:15 +0100

    gerüst test

---
+++ src/src/main/webapp/app/view1/view1.js
@@ -0,0 +1,29 @@
+(function() {
+	'use strict';
+
+	angular.module('myApp.view1', ['ngRoute'])
+
+	.config(['$routeProvider', function($routeProvider) {
+		$routeProvider.when('/view1', {
+			templateUrl: 'view1/view1.html',
+			controller: 'View1Ctrl'
+		});
+	}])
+
+	.controller('View1Ctrl', View1Ctrl);
+
+	View1Ctrl.$inject = ['$http'];
+
+	function View1Ctrl($http) {
+		var vm = this;
+		vm.blubbiblub = "";
+		
+		$http.get('http://localhost:8080/freezers/api/freezer').then(function(resp) {
+			vm.blubbiblub = resp.data.test;
+		}, function(error) {
+			console.dir(error);
+			alert("err");
+		});
+	}
+
+})();

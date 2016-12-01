78c09d3a9d604f366f803a99cc72da9d7792723f
phi <philipwaritschlager@web.de>
2016-12-01 11:53:44 +0100

    ordnerstruktur

---
+++ code/supermarket/src/main/java/ip/freezers/MongoProvider.java
@@ -0,0 +1,25 @@
+package ip.freezers;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Lock;
+import javax.ejb.LockType;
+import javax.ejb.Singleton;
+
+import com.mongodb.MongoClient;
+
+@Singleton
+public class MongoProvider {
+	
+	private MongoClient mongoClient = null;
+	
+	@Lock(LockType.READ)
+	public MongoClient getMongoClient() {
+		return this.mongoClient;
+	}
+	
+	@PostConstruct
+	public void init() {
+		this.mongoClient = new MongoClient("localhost", 27017);
+	}
+
+}
---
+++ code/supermarket/src/main/webapp/app/view2/view2.js
@@ -0,0 +1,25 @@
+(function() {
+	'use strict';
+
+	angular.module('myApp.view2', ['ngRoute'])
+
+	.config(['$routeProvider', function($routeProvider) {
+		$routeProvider.when('/view2', {
+			templateUrl: 'view2/view2.html',
+			controller: 'View2Ctrl'
+	});
+	}])
+	.controller('View2Ctrl', View2Ctrl);
+
+	View2Ctrl.$inject = [];
+
+	function View2Ctrl() {
+		var vm = this;
+		vm.blaaa = blaaa;
+
+		function blaaa() {
+			alert("blaaa");
+		}
+	}
+
+})();

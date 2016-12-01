eb77d309285ecc8cfbc2725862038b8bcf661da5
phi <philipwaritschlager@web.de>
2016-12-01 14:51:20 +0100

    mongo db anbindung test

--- code/fridge/src/main/webapp/app/view1/view1.js
+++ code/fridge/src/main/webapp/app/view1/view1.js
@@ -16,15 +16,15 @@
 
 	function View1Ctrl($http) {
 		var vm = this;
-		vm.blubbiblub = "testiii2";
-		/*
+		vm.blubbiblub = "lädt";
+		
 		$http.get('http://localhost:8080/freezers/api/freezer').then(function(resp) {
 			vm.blubbiblub = resp.data.test;
 		}, function(error) {
 			console.dir(error);
 			alert("err");
 		});
-		*/
+		
 	}
 
 })();

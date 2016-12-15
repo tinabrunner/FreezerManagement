c3d302dd2517755b995e2f05985030e384b4ee62
MariusKoch <marius-koch94@gmx.de>
2016-12-15 12:23:27 +0100

    Invoice Controller

---
+++ code/fridge/src/main/java/controller/InvoiceController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import java.util.List;
+
+import model.Invoice;
+
+public class InvoiceController {
+	
+	private List<Invoice> invoices;
+	
+	public void receiveInvoice(){
+		
+	}
+	
+	
+}
---
+++ code/fridge/src/main/java/model/Invoice.java
@@ -0,0 +1,55 @@
+package model;
+
+import java.util.Date;
+
+public class Invoice {
+	private String id;
+	private String name;
+	private Date billingDate;
+	private Date orderDate;
+	private double totalPrice;
+	private String invoiceURL;
+	
+	
+	public Invoice(String id, String name, Date billingDate, Date orderDate, double totalPrice, String invoiceURL) {
+		this.id = id;
+		this.name = name;
+		this.billingDate = billingDate;
+		this.orderDate = orderDate;
+		this.totalPrice = totalPrice;
+		this.invoiceURL = invoiceURL;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public Date getBillingDate() {
+		return billingDate;
+	}
+
+
+	public Date getOrderDate() {
+		return orderDate;
+	}
+
+
+	public double getTotalPrice() {
+		return totalPrice;
+	}
+
+
+	public String getInvoiceURL() {
+		return invoiceURL;
+	}
+	
+	
+	
+}

1b7204ee7ca804e0ca71b1d0fcf9e1407317f7cd
MariusKoch <marius-koch94@gmx.de>
2016-12-16 12:35:40 +0100

    JMS Invoice

--- code/supermarket/src/main/java/controller/InvoiceController.java
+++ code/supermarket/src/main/java/controller/InvoiceController.java
@@ -1,8 +1,5 @@
 package controller;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 import javax.jms.Queue;
 import javax.jms.QueueConnection;
 import javax.jms.QueueConnectionFactory;
@@ -12,42 +9,52 @@ import javax.jms.Session;
 import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 
+import com.google.gson.Gson;
+
 import Model.Invoice;
 
 public class InvoiceController {
 
 	public void sendInvoice(Invoice invoice) {
-		try { // Create and start connection
+		String message = invoiceToString(invoice);
+
+		try {
+			// 1) Create and start connection
 			InitialContext ctx = new InitialContext();
 			QueueConnectionFactory f = (QueueConnectionFactory) ctx.lookup("myQueueConnectionFactory");
 			QueueConnection con = f.createQueueConnection();
 			con.start();
+
 			// 2) create queue session
 			QueueSession ses = con.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+
 			// 3) get the Queue object
 			Queue t = (Queue) ctx.lookup("myQueue");
+
 			// 4)create QueueSender object
 			QueueSender sender = ses.createSender(t);
+
 			// 5) create TextMessage object
 			TextMessage msg = ses.createTextMessage();
 
 			// 6) write message
-			BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
-			while (true) {
-				System.out.println("Enter Msg, end to terminate:");
-				String s = b.readLine();
-				if (s.equals("end"))
-					break;
-				msg.setText(s);
-				// 7) send message
-				sender.send(msg);
-				System.out.println("Message successfully sent.");
-			}
+			msg.setText(message);
+
+			// 7) send message
+			sender.send(msg);
+			System.out.println("Message successfully sent.");
+
 			// 8) connection close
 			con.close();
 		} catch (Exception e) {
 			System.out.println(e);
 		}
+	}
+
+	public String invoiceToString(Invoice invoice) {
+		Gson gson = new Gson();
+		String jsonInString = gson.toJson(invoice);
+		return jsonInString;
 	}
 
 }

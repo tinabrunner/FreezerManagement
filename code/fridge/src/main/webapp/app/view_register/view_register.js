251b18a13022b61c8c92af649dc9acf49b801cf4
tinabrunner <tina@Tinas-MacBook-Pro.local>
2017-01-03 18:49:27 +0100

    view_register Änderungen

--- code/fridge/src/main/webapp/app/view_register/view_register.js
+++ code/fridge/src/main/webapp/app/view_register/view_register.js
@@ -13,7 +13,33 @@
 			controller: 'view_register1Ctrl'
 		});
 	}])
+	.controller('view_register1Ctrl', function($scope) {
+		$scope.Register = function() {
+			if (!$scope.username || !$scope.password || !$scope.password_confirm || !$scope.firstname || !$scope.lastname || !$scope.email)
+				alert("Please fill all fields!");
+			else if ($scope.password != $scope.password_confirm)
+				alert("The passwords don't match!");
+			else {
+				var username = $scope.username;
+				var password = $scope.password;
+				var password_confirm = $scope.password_confirm;
+				var firstname = $scope.firstname;
+				var lastname = $scope.lastname;
+				var email = $scope.email;
+				alert("You will be registered now!");
+								
+				// kommunikation mit java controller !!
+				// --> wenn Register-Methode TRUE zurück gibt: einloggen und redirectTo: '/view1';
+				// --> wenn FALSE zur erneuten Eingabe auffordern
+				
+				// var accountController = new java.controller.AccountController();
+			}
+	    }
+	});
+	
+})();
 
+/*
 	.controller('view_register1Ctrl', RegisterCtrl);
 
 	RegisterCtrl.$inject = ['$http'];
@@ -23,7 +49,5 @@
 		var vm = this;
 
 	};
-	
-
-
 })();
+*/

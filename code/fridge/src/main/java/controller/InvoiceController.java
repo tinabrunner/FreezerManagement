530640a4febb0bf99cee65cb8364a891f68cf715
MariusKoch <marius-koch94@gmx.de>
2017-01-04 13:00:38 +0100

    Invoice Controller

--- code/fridge/src/main/java/controller/InvoiceController.java
+++ code/fridge/src/main/java/controller/InvoiceController.java
@@ -2,40 +2,15 @@ package controller;
 
 import java.util.List;
 
-import javax.ejb.Stateless;
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.MessageListener;
-import javax.jms.Queue;
-import javax.jms.QueueConnection;
-import javax.jms.QueueConnectionFactory;
-import javax.jms.QueueReceiver;
-import javax.jms.QueueSession;
-import javax.jms.Session;
-import javax.jms.TextMessage;
-import javax.naming.InitialContext;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
-import com.google.gson.Gson;
-
 import model.Invoice;
 
-@Stateless
-@Path("/invoices")
-@Produces(MediaType.APPLICATION_JSON)
 public class InvoiceController {
 
 	private List<Invoice> invoices;
 
-
-	@POST
 	public static void addInvoice(Invoice invoice) {
 		// invoices.add(invoice);
 	}
-	@POST
 	public List<Invoice> getInvoices() {
 		return invoices;
 	}

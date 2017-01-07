57509a8bca95f3a55c1a046b639a1b4af31810b1
JD <jana.dreiling@stud.fh-rosenheim.de>
2017-01-07 20:20:36 +0100

    [1] changed pom, because ClassFormatError [2] shoppinglist.html dummy

--- code/fridge/src/main/java/model/ShoppingListItem.java
+++ code/fridge/src/main/java/model/ShoppingListItem.java
@@ -1,48 +1,49 @@
 package model;
 
-import java.util.Date;
-
 /**
  * Created by JD on 08.12.2016.
  */
 public class ShoppingListItem extends ProductCategory { // todo id foreignkey ?
 
-	public ShoppingListItem() {} // keep
-	
-    public ShoppingListItem( String id, double preis, String name, int verpackungsGroesse,
-                             int mindestBestand, int hoechstBestand, boolean regelmaessig) {
-		super(id, preis,name,verpackungsGroesse);
-		
+	public ShoppingListItem() {
+	} // keep
+
+	public ShoppingListItem(String id, double preis, String name, int verpackungsGroesse, int mindestBestand,
+			int hoechstBestand, boolean regelmaessig) {
+
+		super(id, preis, name, verpackungsGroesse);
+
 		this.mindestBestand = mindestBestand;
 		this.hoechstBestand = hoechstBestand;
 		this.regelmaessig = regelmaessig;
 	}
-	
+
 	// todo categoryId ? s.a. inventoryProduct
-	private int mindestBestand = 0;
-	private int hoechstBestand = 0;
-	private boolean regelmaessig = false;
-	
+
+	private int mindestBestand;
+	private int hoechstBestand;
+	private boolean regelmaessig;
+
 	public int getMindestBestand() {
 		return mindestBestand;
 	}
-	
+
 	public void setMindestBestand(int mindestBestand) {
 		this.mindestBestand = mindestBestand;
 	}
-	
+
 	public int getHoechstBestand() {
 		return hoechstBestand;
 	}
-	
+
 	public void setHoechstBestand(int hoechstBestand) {
 		this.hoechstBestand = hoechstBestand;
 	}
-	
+
 	public boolean isRegelmaessig() {
 		return regelmaessig;
 	}
-	
+
 	public void setRegelmaessig(boolean regelmaessig) {
 		this.regelmaessig = regelmaessig;
 	}

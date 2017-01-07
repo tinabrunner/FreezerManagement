60cabb392edaeab6ff0236575d5a2bcd2a4f795d
phi <philipwaritschlager@web.de>
2017-01-07 11:23:57 +0100

    dummer glassfish abfuck

--- code/fridge/src/main/java/controller/ShoppingCartController.java
+++ code/fridge/src/main/java/controller/ShoppingCartController.java
@@ -1,18 +1,16 @@
 package controller;
 
-import db_communication.DB_ShoppingList;
 import domain.MongoProvider;
-import domain.MongoProvider2;
 import domain.Product;
-import repository.ShoppingListRepositoryMongoImpl;
-import service.ShoppingListServiceImpl;
 
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.ejb.Stateless;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.util.Map;
+import java.text.ParseException;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by phi on 08.12.16.
@@ -32,19 +30,32 @@ public class ShoppingCartController {
 	
 	@GET
 	/** Shopping carts (Warenkörbe):  non-persistent */
-	public String createShoopingCart() {
+	public Product createShoopingCart() throws ParseException {
 		
-		DB_ShoppingList db_shoppingList = new DB_ShoppingList();
-		// Map<Product, Integer> shoppingListProducts = db_shoppingList.getAllProductsFromShoppingList(); // miau
+		//DB_ShoppingList db_shoppingList = new DB_ShoppingList();
+		//Map<Product, Integer> shoppingListProducts = db_shoppingList.getAllProductsFromShoppingList(); // miau
 		
-		// bsp
-		/*MongoClient mongoClient = this.mongoProvider.getMongoClient();
-		MongoDatabase db = mongoClient.getDatabase("fridge");
-		MongoCollection<Document> col = db.getCollection("shoppingListProducts");
-		Document d = col.find().first();
-		return d.toJson();*/
+		Set<Product> shoppingListProducts = new HashSet<>();
+		shoppingListProducts.add(new Product(1.99d,"Belegte Banane", 1,
+				1, 2, 9999,
+				true, null, "1"));
+		shoppingListProducts.add(new Product(2.99d,"Abenteurlicher Apfel", 1,
+				5, 10, 9999,
+				true, null, "2"));
+		shoppingListProducts.add(new Product(458.99d,"Emmentaler", 1,
+				-1, 1, 9999,
+				false, null, "3"));
+		shoppingListProducts.add(new Product(0.45d,"Gouda", 1,
+				-1, 2, 9999,
+				false, null, "4"));
+		shoppingListProducts.add(new Product(2.99d,"Eier", 6,
+				6, 10, 9999,
+				true, null, "5"));
 		
-		return "123";
+		//return shoppingListProducts.toArray(new Product[]{})[0];
+		return new Product(458.99d,"Emmentaler", 1,
+				-1, 1, 9999,
+				false, null, "3");
 	}
 }
 

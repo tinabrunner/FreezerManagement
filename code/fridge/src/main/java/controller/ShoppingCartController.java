29de9469758023d04899a794d8e60e7e361d6b56
phi <philipwaritschlager@web.de>
2017-01-09 01:21:22 +0100

    merge

--- code/fridge/src/main/java/controller/ShoppingCartController.java
+++ code/fridge/src/main/java/controller/ShoppingCartController.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.text.ParseException;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import javax.ejb.EJB;
@@ -12,9 +13,20 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import db_communication.DB_FridgeInventory;
 import db_communication.DB_ShoppingList;
 import domain.MongoProvider;
+import domain.MongoProvider2;
+import model.InventoryProduct;
 import model.ShoppingListItem;
+import org.bson.Document;
+import repository.ShoppingListRepositoryMongoImpl;
+import service.ShoppingListServiceImpl;
+import util.ShoppingListHelper;
 
 /**
  * Created by phi on 08.12.16.
@@ -36,17 +48,26 @@ public class ShoppingCartController {
 	/** Shopping carts (Warenkörbe): non-persistent */
 	public Set<ShoppingListItem> createShoopingCart() throws ParseException {
 
+		/* current items in shopping list */
 		DB_ShoppingList db_shoppingList = new DB_ShoppingList();
 		Set<ShoppingListItem> shoppingListProducts = db_shoppingList.getAllProductsFromShoppingList(); // miau
-
-		/*
-		Set<ShoppingListItem> shoppingListProducts = new HashSet<>();
-		shoppingListProducts.add(new ShoppingListItem("i1", 1.99d, "Belegte Banane", 1, 1, 2, true));
-		shoppingListProducts.add(new ShoppingListItem("i2", 2.99d, "Abenteurlicher Apfel", 1, 5, 10, true));
-		shoppingListProducts.add(new ShoppingListItem("i3", 458.99d, "Emmentaler", 1, -1, 1, false));
-		shoppingListProducts.add(new ShoppingListItem("i4", 0.45d, "Gouda", 1, -1, 2, false));
-		shoppingListProducts.add(new ShoppingListItem("i4", 2.99d, "Eier", 6, 6, 10, true));
-		*/
+		
+		/* current items in inventory: <categoryId, inventoryProduct> */
+		DB_FridgeInventory db_fridgeInventory = new DB_FridgeInventory();
+		Map<String, InventoryProduct> inventoryProducts = db_fridgeInventory.getAllProducts();
+		
+/*
+		MongoProvider2 mongoProvider = new MongoProvider2("localhost", 27017);
+		mongoProvider.setDatabaseName("fridge");
+		mongoProvider.connect();
+		ShoppingListRepositoryMongoImpl shoppingListRepositoryMongo = new ShoppingListRepositoryMongoImpl(mongoProvider);
+		ShoppingListServiceImpl shoppingListService = new ShoppingListServiceImpl(shoppingListRepositoryMongo);
+		shoppingListService.addProduct(new ShoppingListItem("i1", 1.99d, "Belegte Banane", 1, 1, 2, true));
+		shoppingListService.addProduct(new ShoppingListItem("i2", 2.99d, "Abenteurlicher Apfel", 1, 5, 10, true));
+		shoppingListService.addProduct(new ShoppingListItem("i3", 458.99d, "Emmentaler", 1, -1, 1, false));
+		shoppingListService.addProduct(new ShoppingListItem("i4", 0.45d, "Gouda", 1, -1, 2, false));
+		shoppingListService.addProduct(new ShoppingListItem("i4", 2.99d, "Eier", 6, 6, 10, true));
+*/
 		return shoppingListProducts;
 	}
 }

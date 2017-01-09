5606f3aaf7443306e342563611932fa46686498b
MariusKoch <marius-koch94@gmx.de>
2017-01-09 13:36:16 +0100

    Supermarket main

---
+++ code/supermarket/src/main/java/supermarket/SupermarketManager.java
@@ -0,0 +1,24 @@
+package supermarket;
+
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+/**
+ * @author Marius Koch
+ *
+ */
+@Singleton
+@Startup
+public class SupermarketManager {
+	//
+	// /**
+	// * @param args
+	// * @throws JMSException
+	// */
+	//
+	// public static void main(String[] args) throws JMSException {
+	// System.out.println("Main läuft");
+	// OrderReceiver orderReceiver = new OrderReceiver();
+	// }
+
+}

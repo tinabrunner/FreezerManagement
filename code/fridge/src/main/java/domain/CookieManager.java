298019b588891aefdc630696933de602e46efcfd
tinabrunner <tina@Tinas-MacBook-Pro.local>
2017-01-12 08:26:13 +0100

    cookie manager geht noch nicht

--- code/fridge/src/main/java/domain/CookieManager.java
+++ code/fridge/src/main/java/domain/CookieManager.java
@@ -1,14 +1,6 @@
 package domain;
 
-import java.util.List;
-
-import org.apache.http.HttpResponse;
 import org.apache.http.client.CookieStore;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.cookie.Cookie;
-import org.apache.http.impl.client.BasicCookieStore;
-import org.apache.http.impl.client.HttpClientBuilder;
 
 public class CookieManager {
 
@@ -16,32 +8,25 @@ public class CookieManager {
 
 	public void main(String args[]) throws Exception {
 
-		// init client
-		HttpClient client = null;
-		httpCookieStore = new BasicCookieStore();
-		HttpClientBuilder builder = HttpClientBuilder.create().setDefaultCookieStore(httpCookieStore);
-		client = builder.build();
-
-		// do stuff
-		HttpGet httpRequest = new HttpGet("http://localhost:8080/fridge/app/");
-		HttpResponse httpResponse = null;
-		try {
-			httpResponse = client.execute(httpRequest);
-		} catch (Throwable error) {
-			throw new RuntimeException(error);
-		}
-
-	}
-
-	public boolean isUserInCookies(String username) {
-		boolean ret = false;
-		// check cookies
-		List<Cookie> cookies = httpCookieStore.getCookies();
-		for (Cookie cookie : cookies) {
-			if (cookie.getName().equals(username))
-				ret = true;
-		}
-		return ret;
+		/*
+		 * // init client HttpClient client = null; httpCookieStore = new
+		 * BasicCookieStore(); HttpClientBuilder builder =
+		 * HttpClientBuilder.create().setDefaultCookieStore(httpCookieStore);
+		 * client = builder.build();
+		 * 
+		 * // do stuff HttpGet httpRequest = new
+		 * HttpGet("http://localhost:8080/fridge/app/"); HttpResponse
+		 * httpResponse = null; try { httpResponse =
+		 * client.execute(httpRequest); } catch (Throwable error) { throw new
+		 * RuntimeException(error); }
+		 * 
+		 * }
+		 * 
+		 * public boolean isUserInCookies(String username) { boolean ret =
+		 * false; // check cookies List<Cookie> cookies =
+		 * httpCookieStore.getCookies(); for (Cookie cookie : cookies) { if
+		 * (cookie.getName().equals(username)) ret = true; } return ret;
+		 */
 	}
 
 }

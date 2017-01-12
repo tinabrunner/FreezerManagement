adf5f1aa76bbbc1d7140a88d860d009927bff0d1
tinabrunner <tina@Tinas-MacBook-Pro.local>
2017-01-12 08:16:02 +0100

    session store weiter gemacht ...

---
+++ code/fridge/src/main/java/db_communication/DB_UserSessionStore.java
@@ -0,0 +1,116 @@
+package db_communication;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+
+import domain.MongoProvider;
+import model.UserSessionData;
+
+@Stateless
+public class DB_UserSessionStore {
+
+	@EJB
+	private MongoProvider mongoProvider;
+
+	private static final String _fridge = "fridge";
+	private static final String _userSessionStore = "userSessionStore";
+	public static final String _token = "token";
+	public static final String _username = "username";
+
+	/*
+	 * STANDARDMETHODS FOR RE-USE
+	 */
+
+	// create a client and get the database and _userSessionStoreCollection
+	private MongoCollection<Document> getSessionStore() {
+		MongoClient mongoClient = this.mongoProvider.getMongoClient();
+		MongoDatabase db = mongoClient.getDatabase(_fridge);
+
+		MongoCollection<Document> sessionStore = db.getCollection(_userSessionStore);
+
+		return sessionStore;
+	}
+
+	private Document convertSessionDataToDocument(UserSessionData data) {
+		Document doc = new Document(_token, data.getToken()).append(_username, data.getUsername());
+		return doc;
+	}
+
+	private UserSessionData convertDocumentToSessionData(Document doc) {
+		String token = doc.getString(_token);
+		String username = doc.getString(_username);
+		return new UserSessionData(token, username);
+	}
+
+	/*
+	 * METHODS TO COMMUNICATE WITH DB
+	 */
+
+	// Method to Insert a UserSession
+	public void insertUserSessionToDB(UserSessionData data) {
+		MongoCollection<Document> sessionStore = getSessionStore();
+		Document doc = convertSessionDataToDocument(data);
+		if (usernameExists(data)) {
+			Bson filter = Filters.eq(_token, data.getToken());
+			sessionStore.findOneAndDelete(filter);
+		}
+		sessionStore.insertOne(doc);
+	}
+
+	// Method to Delete a UserSession
+	public void deleteUserSessionFromDB(String token) {
+		MongoCollection<Document> sessionStore = getSessionStore();
+		Bson filter = Filters.eq(_token, token);
+		sessionStore.findOneAndDelete(filter);
+	}
+
+	// Method to Get a UserSession
+	public UserSessionData getUserSessionFromDB(String token) {
+		MongoCollection<Document> sessionStore = getSessionStore();
+		Bson filter = Filters.eq(_token, token);
+		FindIterable<Document> result = sessionStore.find(filter);
+		return convertDocumentToSessionData(result.first());
+	}
+
+	// Check if Usernamealready exists
+	public boolean usernameExists(UserSessionData data) {
+		MongoCollection<Document> sessionStore = getSessionStore();
+		Bson filter = Filters.eq(_username, data.getUsername());
+		if (sessionStore.count(filter) > 0)
+			return true;
+		else
+			return false;
+	}
+
+	// Check if UserSession already exists
+	public boolean userSessionExists(UserSessionData data) {
+		MongoCollection<Document> sessionStore = getSessionStore();
+		Bson filter = Filters.eq(_username, data.getUsername());
+		FindIterable<Document> result = sessionStore.find(filter);
+		UserSessionData foundUser = convertDocumentToSessionData(result.first());
+		if (foundUser.getToken().equals(data.getToken()))
+			return true;
+		else
+			return false;
+	}
+
+	// Check if Token already exists
+	public boolean tokenExists(String token) {
+		MongoCollection<Document> sessionStore = getSessionStore();
+		Bson filter = Filters.eq(_token, token);
+		if (sessionStore.count(filter) > 0)
+			return true;
+		else
+			return false;
+	}
+
+}
--- code/fridge/src/main/webapp/app/view_login/view_login.js
+++ code/fridge/src/main/webapp/app/view_login/view_login.js
@@ -5,15 +5,15 @@
 (function() {
 	'use strict';
 
-	angular.module('fridge.view_login', ['ngRoute'])
-
+	angular.module('fridge.view_login', ['ngRoute', 'ngCookies'])
+	
 	.config(['$routeProvider', function($routeProvider) {
 		$routeProvider.when('/view_login', {
 			templateUrl: 'view_login/view_login.html',
 			controller: 'view_login1Ctrl'
 		});
 	}])
-	.controller('view_login1Ctrl', function($scope, $http) {
+	.controller('view_login1Ctrl', function($scope, $http, $cookies) {
 		$scope.Login = function() {
 			if (!$scope.username || !$scope.password)
 				alert("Please enter username and password!");
@@ -24,8 +24,7 @@
 		        };
 				$http.post(URL_API+'login', objData).then((function(response){
 					alert("Passt: "+response.data);
-					setCookie(response.data);
-					
+					$cookies.put('username', response.data);
 				}), 
 				function(response) { 
 					alert("Something went wrong: "+response.data);
@@ -34,45 +33,7 @@
 			}
 	    }
 		
-		
-		// Funktions für Cookie Management
-		function setCookie(cname) {
-		    //var d = new Date();
-		    // d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-		    //var expires = "expires="+d.toUTCString();
-		    document.cookie = cname + ";path=/";  // + "=" + cvalue + ";" + expires 
-		}
-
-		function getCookie(cname) {
-		    var name = cname + "=";
-		    var ca = document.cookie.split(';');
-		    for(var i = 0; i < ca.length; i++) {
-		        var c = ca[i];
-		        while (c.charAt(0) == ' ') {
-		            c = c.substring(1);
-		        }
-		        if (c.indexOf(name) == 0) {
-		            return c.substring(name.length, c.length);
-		        }
-		    }
-		    return "";
-		}
-
-		function checkCookie() {
-		    var user = getCookie("username");
-		    if (user != "") {
-		        alert("Welcome again " + user);
-		    } else {
-		        user = prompt("Please enter your name:", "");
-		        if (user != "" && user != null) {
-		            setCookie("username", user, 365);
-		        }
-		    }
-		}
 	});
 	
-	
-
-
 })();
 	
